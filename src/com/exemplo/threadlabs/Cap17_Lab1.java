3da459a1d483b6dde4b7fb0b5fdf450a89939275
marceloscientist <marcelo.scientist@hotmail.com>
2022-05-30 15:27:47 -0300

    Update Cap17_Lab1.java
    Just commenting which lab is about!

--- src/com/exemplo/threadlabs/Cap17_Lab1.java
+++ src/com/exemplo/threadlabs/Cap17_Lab1.java
@@ -4,6 +4,8 @@ public class Cap17_Lab1 {
 
 	public static void main(String[] args) {
 		/*
+		
+		LAB 1
 		ThreadLab1 t1 = new ThreadLab1("Thread 1");
 		ThreadLab1 t2 = new ThreadLab1("Thread 2");
 		
@@ -11,6 +13,11 @@ public class Cap17_Lab1 {
 		t2.start();
 		*/
 		
+
+		/*
+				LAB 2
+
+		*/
 		ThreadLab2 t1 = new ThreadLab2("Thread 1");
 		ThreadLab2 t2 = new ThreadLab2("Thread 2");
 		
